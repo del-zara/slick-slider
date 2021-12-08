5c35025a4281b2b24aeb8208bc110812e1ec8ee4
del-zara <zarina.del@inbox.ru>
2021-12-08 15:35:10 +0600

    slick-slider

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: scroll
+  });
+                
+//   or 
+ 
+// infinite: true
+// autoplay: true 
+// autoplaySpeed: 1000, 
+// slideToSHow: any number 
+// slidesToScroll: any number  
+// dots: any type watch videos
+  
